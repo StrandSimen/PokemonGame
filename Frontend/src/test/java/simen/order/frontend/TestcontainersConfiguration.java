d2d2939102b6740610f0324fb1aaf53c31640c29
simenns <simenns@uio.no>
2025-09-23 11:44:26 +0200

    Fixed the frontend directory

--- Frontend/src/test/java/simen/order/frontend/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package simen.order.frontend;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
